d17d0193e12fa889730a66d6cbdfa4f091fda211
payalpatra <payalpatra105@gmail.com>
2021-04-28 09:41:41 +0530

    Added store to local Storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,31 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, composeWithDevTools());
+function saveToLocalStorage(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+function loadFromLocalStorage(state) {
+    try {
+        const serializedState = localStorage.getItem("state");
+        if (serializedState === null) return undefined 
+        return JSON.parse(serializedState)
+     
+    } catch (e) {
+        console.log(e)
+        return undefined
+    }
+}
+
+const persistedState = loadFromLocalStorage()
+const store = createStore(rootReducer, persistedState,composeWithDevTools());
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
